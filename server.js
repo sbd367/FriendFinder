6cdb9a49e394384b5f19ecbeb6052fa73bac2417
meh <spencerdberg1@gmail.com>
2018-10-25 18:09:13 -0500

    commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,7 +19,7 @@ var htmlR = require("./routing/htmlRoutes")
 htmlR(app);
 
 
-app.listen(PORT, function() {
+app.listen(port, function() {
     // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+    console.log("Server listening on: http://localhost:" + port);
   });
